e46eb900edcb612e8fcc243db54513331892932e
CARLOS EDUARDO DA SILVA FILHO <23111777@dsn127m1246726.cfp127aluno.local>
2024-03-12 11:48:26 -0300

    telas finalizadas

--- app/src/main/java/br/senai/sp/jandira/triproom/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/triproom/MainActivity.kt
@@ -4,15 +4,20 @@ import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Arrangement.End
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -39,8 +44,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -177,6 +184,7 @@ fun Greeting() {
                 )
             Text(
                 text = "Sign up",
+                fontWeight = FontWeight.Bold,
                 color = Color(0xffCF06F0)
             )
         }
@@ -211,21 +219,62 @@ fun SignUp () {
                 .align(Alignment.End)
         ){
         }
+        Spacer(modifier = Modifier.height(10.dp))
         Column(
-            modifier = Modifier.align(Alignment.CenterHorizontally)
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxWidth()
         ) {
         Text(text = "Sign in",
             fontSize = 32.sp,
             color = Color(0xffCF06F0),
             modifier = Modifier
                 .align(Alignment.CenterHorizontally),
-            fontWeight = FontWeight.Bold
+            fontWeight = FontWeight.ExtraBold
 
         )
         Text(
             text = "Create a new account",
             color = Color(0xffA09C9C)
         )
+            Spacer(modifier = Modifier.height(10.dp))
+            Box (
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ){
+                Image(painter = painterResource(id = R.drawable.foto),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .width(30.dp)
+                        .height(30.dp)
+                        .offset(x = 55.dp, y = 35.dp)
+                )
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                    .height(100.dp)
+                        .width(100.dp)
+                    .border(
+                        BorderStroke(
+                            2.dp, Brush.horizontalGradient(
+                                0.2f to Color(0xffCF06F0),
+                                0.6f to Color.White,
+                                0.4f to Color.White,
+                                startX = 0.0f,
+                                endX = 430.0f
+                            )
+                        ), shape = RoundedCornerShape(360.dp)
+                    )
+                    //.background(color = Color.White)
+                    ) {
+                    androidx.compose.material3.Icon(
+                        imageVector = Icons.Filled.Person,
+                        contentDescription = "Foto De Perfil",
+                        tint = Color(0xffCF06F0),
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(40.dp))
         }
         OutlinedTextField(
             value = "Susanna Hoffs" ,
@@ -335,6 +384,69 @@ fun SignUp () {
             }
         )
         Spacer(modifier = Modifier.height(20.dp))
+        Row(
+            modifier = Modifier
+                .padding(start = 24.dp, end = 24.dp)
+                .align(Alignment.Start)
+        ) {
+            Row (
+                modifier = Modifier
+                    .height(30.dp)
+                    .width(30.dp)
+                    .border(
+                        width = 2.dp, color = Color(0xffCF06F0)
+                    )
+            ){}
+            Text(
+                text = "Over 18?",
+                modifier = Modifier.padding(top = 8.dp, start = 10.dp)
+            )
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(Color(0xffCF06F0)),
+            shape = RoundedCornerShape(10.dp),
+            modifier = Modifier
+                .align(Alignment.End)
+                .padding(end = 24.dp, start = 24.dp)
+                .fillMaxWidth()
+        ) {
+            Text(
+                text ="CREATE ACCOUNT",
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp
+            )
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        Row (
+            modifier = Modifier
+                .align(Alignment.End)
+                .padding(end = 24.dp)
+        ){
+            Text(
+                text = "Already have an account?",
+                color = Color(0xffA09C9C)
+            )
+            Text(
+                text = "Sign in",
+                fontWeight = FontWeight.Bold,
+                color = Color(0xffCF06F0)
+            )
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        Row(
+            modifier = Modifier
+                .width(150.dp)
+                .height(50.dp)
+                .background(
+                    Color(0xffCF06F0),
+                    shape = RoundedCornerShape(topEnd = 16.dp)
+                )
+                .align(Alignment.Start)
+
+        ){
+        }
 
     }
 }
